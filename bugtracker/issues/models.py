2d2f0ee46a3c7db9e35145fa84d77fc79d516eb7
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-06-29 21:40:59 +0200

    adding db models

--- bugtracker/issues/models.py
+++ bugtracker/issues/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from datetime import date
+from django.urls import reverse
+from django.conf import settings
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Project(models.Model):
+    name = models.CharField(max_length=63)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    public = models.BooleanField(default=False)
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(null=True, auto_now=True)
+    project = models.OneToOneField(Project, blank=False, related_name="tickets")
