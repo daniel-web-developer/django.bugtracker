9aae30dae3d96c9329a0c65e4ccda2aad971ae15
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-06-30 14:27:24 +0200

    new view

--- bugtracker/issues/views.py
+++ bugtracker/issues/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index/index.html')
+
+def new(request):
+    if request.user.is_authenticated:
+        return render(request, 'new/index.html')
+    else:
+        return render(request, 'registration/need_login.html')
